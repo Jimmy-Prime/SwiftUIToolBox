54a3a8500ab76a22d77f9b047df66f39749feaa8
Jimmy-Lee <jimmylevelup@gmail.com>
2019-12-06 17:34:28 +0800

    remove unnecessary import Foundation

--- SwiftUIToolBox/Expandable Cards/CardContent.swift
+++ SwiftUIToolBox/Expandable Cards/CardContent.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 struct CardContent: Identifiable {
     let title: String
     let description: String
--- SwiftUIToolBox/Transition Animation/Track.swift
+++ SwiftUIToolBox/Transition Animation/Track.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 struct Track: Identifiable, Codable {
     let artist: String
     let name: String
